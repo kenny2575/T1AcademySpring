295395cae131b9a8b95321428baae952e41b1174
DESKTOP-3HAS177\rusni <rus.nigmat@gmail.com>
2025-02-18 22:25:32 +0700

    Установка параметров через @Value

--- src/main/java/org/example/configuration/DatabaseConfig.java
+++ src/main/java/org/example/configuration/DatabaseConfig.java
@@ -2,23 +2,42 @@ package org.example.configuration;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 
 import javax.sql.DataSource;
+import java.io.IOException;
 
 @Configuration
+@PropertySource("classpath:application.yml")
 public class DatabaseConfig {
 
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
     @Bean
-    public DataSource dataSource(){
+    public DataSource dataSource() {
 
         var hikariConfig = new HikariConfig();
         hikariConfig.setPoolName("postgresPool");
-        hikariConfig.setDriverClassName("org.postgresql.Driver");
-        hikariConfig.setJdbcUrl("jdbc:postgresql://localhost:5432/Study_DB");
-        hikariConfig.setUsername("GodOmen");
-        hikariConfig.setPassword("GodOmen");
+        hikariConfig.setDriverClassName(driverClassName);
+        hikariConfig.setJdbcUrl(url);
+        hikariConfig.setUsername(username);
+        hikariConfig.setPassword(password);
 
         hikariConfig.setMaximumPoolSize(10);
         hikariConfig.setMinimumIdle(2);
@@ -27,5 +46,14 @@ public class DatabaseConfig {
         hikariConfig.setConnectionTimeout(3000);
 
         return new HikariDataSource(hikariConfig);
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigurer() throws IOException {
+        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+        YamlPropertiesFactoryBean yamlFactory = new YamlPropertiesFactoryBean();
+        yamlFactory.setResources(new ClassPathResource("application.yml"));
+        configurer.setProperties(yamlFactory.getObject());
+        return configurer;
     }
 }
