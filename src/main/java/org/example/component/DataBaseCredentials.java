21a536579db9a99932ebaaae0a4a3f611f945e0a
DESKTOP-3HAS177\rusni <rus.nigmat@gmail.com>
2025-02-25 14:40:03 +0700

    Исправлен запрос user, для корректной работы при повторном запуске.

---
+++ src/main/java/org/example/component/DataBaseCredentials.java
@@ -0,0 +1,11 @@
+package org.example.component;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataBaseCredentials {
+
+    public DataBaseCredentials() {};
+
+}
