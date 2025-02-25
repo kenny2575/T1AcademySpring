f6e511477052c69b09acd38c2f678c327d04ec94
DESKTOP-3HAS177\rusni <rus.nigmat@gmail.com>
2025-02-25 20:54:48 +0700

    Удален конфликтующие зависимости

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,5 +1,26 @@
 package org.example.model;
 
-public record User (Long id, String username) {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", username='" + username + "'}";
+    }
 }
