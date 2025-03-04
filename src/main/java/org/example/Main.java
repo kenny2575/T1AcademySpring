594b52b1046afe8e690668a35e7e1a8f28d2bd27
DESKTOP-3HAS177\rusni <rus.nigmat@gmail.com>
2025-03-04 18:47:37 +0700

    Примеры использования

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.model.User;
 import org.example.service.UserService;
 import org.springframework.boot.CommandLineRunner;
@@ -8,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
@@ -17,9 +19,15 @@ public class Main {
     @Bean
     CommandLineRunner commandLineRunner (UserService userService) {
         return args -> {
-            userService.createUser(new User(null, "user1"));
-            userService.createUser(new User(null, "user2"));
-            userService.getAllUsers().forEach(System.out::println);
+            var user1 = new User(null, "user1");
+            var user2 = new User(null, "user2");
+            userService.createUser(user1);
+            userService.createUser(user2);
+            userService.getAllUsers().forEach(x -> log.info(x.toString()));
+
+            var newUser = userService.getUser(42L);
+
+            log.info("Name = {}",  newUser.getId());
         };
     }
 
--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -4,19 +4,28 @@ package org.example.service;
 import org.example.model.User;
 import org.example.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+
 public class UserService {
     private final UserRepository userRepository;
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+    @Transactional
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
-    public User createUser(User user) {
-        return userRepository.save(user);
+    @Transactional
+    public void createUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public User getUser(Long id) {
+        return userRepository.getReferenceById(id);
     }
 }
