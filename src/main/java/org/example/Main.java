235fb6e20674ba1f2356fac387e7ed68f30254bd
DESKTOP-3HAS177\rusni <rus.nigmat@gmail.com>
2025-03-10 00:04:08 +0700

    Вынес в отдельный компонент

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,7 @@ package org.example;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.example.enums.ProductType;
 import org.example.model.User;
-import org.example.service.ProductService;
 import org.example.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -18,21 +16,6 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
-    @Bean
-    CommandLineRunner commandLineRunner (UserService userService, ProductService productService) {
-        return args -> {
-            User user1 = userService.createUser(new User("Alice"));
-            User user2 = userService.createUser(new User("Bob"));
-
-            productService.createProduct(user1.getId(), ProductType.CARD);
-            productService.createProduct(user1.getId(), ProductType.ACCOUNT);
-            productService.createProduct(user2.getId(), ProductType.CARD);
-
-            System.out.println("Products for Alice: " + productService.getProductsByUserId(user1.getId()));
-            System.out.println("Products for Bob: " + productService.getProductsByUserId(user2.getId()));
-
-        };
-    }
 
 }
 
