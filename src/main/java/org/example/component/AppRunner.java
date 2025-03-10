88eed75df60954bd5ffd5adaf43362277e03ac3f
DESKTOP-3HAS177\rusni <rus.nigmat@gmail.com>
2025-03-10 14:38:05 +0700

    Демонстрация работы через CommandLineRunner

--- src/main/java/org/example/component/AppRunner.java
+++ src/main/java/org/example/component/AppRunner.java
@@ -2,7 +2,7 @@ package org.example.component;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.example.model.User;
+import org.example.service.ProductService;
 import org.example.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +12,21 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class AppRunner implements CommandLineRunner {
 
+    private final ProductService productService;
     private final UserService userService;
 
     @Override
     public void run(String... args) throws Exception {
-        var user1 = new User(null, "user1");
-        var user2 = new User(null, "user2");
-        userService.createUser(user1);
-        userService.createUser(user2);
-        userService.getAllUsers().forEach(x -> log.info(x.toString()));
+
+        var users = userService.getAllUsers();
+        var user = users.stream().filter(x -> "Bob".equals(x.getUsername())).findFirst();
+
+        user.ifPresent(
+                us -> log.info("Products for {} [{}] : {}", us.getUsername(), us.getId(), productService.getProductsByUserId(us.getId()))
+        );
+
+        var product = productService.getProductById(105L);
+        product.ifPresent(pr -> log.info("Product: {}", pr));
+
     }
 }
